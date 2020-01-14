6d08b9b5b72d55e925bcd1ed18908136cd02394c
mingkunhe <hmkhaha@gmail.com>
2020-01-13 18:27:45 -0700

    add mulpti markers

--- src/main/webapp/app/my-map/my-map.component.ts
+++ src/main/webapp/app/my-map/my-map.component.ts
@@ -34,6 +34,31 @@ export class MyMapComponent implements AfterViewInit {
     title: 'Hello World!'
   });
 
+
+  coordinates1 = new google.maps.LatLng(40.70463334, -74.01361706);
+  marker1 = new google.maps.Marker({
+    position: this.coordinates1,
+    map: this.map,
+    title: 'Broadway & Battery Pl'
+  });
+
+  coordinates2 = new google.maps.LatLng(40.75510267, -73.97498696);
+  marker2 = new google.maps.Marker({
+    position: this.coordinates2,
+    map: this.map,
+    title: 'E 47 St & Park Ave'
+  });
+
+  coordinates3 = new google.maps.LatLng(40.75828065, -73.97069431);
+  marker3 = new google.maps.Marker({
+    position: this.coordinates3,
+    map: this.map,
+    title: 'E 53 St & Lexington Ave'
+  });
+
+
+
+
   ngAfterViewInit() {
     this.mapInitializer();
   }
@@ -41,5 +66,8 @@ export class MyMapComponent implements AfterViewInit {
   mapInitializer() {
     this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
     this.marker.setMap(this.map);
+    this.marker1.setMap(this.map);
+    this.marker2.setMap(this.map);
+    this.marker3.setMap(this.map);
   }
 }
